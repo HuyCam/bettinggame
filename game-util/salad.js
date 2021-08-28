ee01ff6072a3d96f86b75cfc9222917c443b971b
huy cam <camhuy165@gmail.com>
2021-08-27 21:10:25 -0500

    stable release 2 add more variety of hitting chance

--- game-util/salad.js
+++ game-util/salad.js
@@ -42,14 +42,14 @@ const saladGame = {
     restTimer: 10000,
     last8Results: new Array(),
     lastResult:"",
+    roundCount: 0,
+    roundCap: 5,
+    getResultType: 1,
     initiateGame: function() {
         this.nextDrawTime = this.getNextDrawTime() + this.restTimer;
         this.allowBet = true;
     },
-    getNextDrawTime() {
-        return new Date().getTime() + this.drawTimer;
-    },
-    pickItemType : function() {
+    getResultTypeZero() {
         let randomVal = getRandomInt(gameSetting.MAX_RANDOMIZED);
         let result = '';
 
@@ -71,6 +71,104 @@ const saladGame = {
             result = BET_ITEM_TYPE.HIGH_YIELD_ITEM.LION;
         }
 
+        return result;
+    },
+    getResultTypeOne() {
+        // high yield lower bandwith have almost as many as low yield
+        let randomVal = getRandomInt(gameSetting.MAX_RANDOMIZED);
+        let result = '';
+
+        if (randomVal <= 149) {
+            result = BET_ITEM_TYPE.LOW_YIELD_ITEM.BULL;
+        } else if (randomVal > 149 && randomVal <= 299) {
+            result = BET_ITEM_TYPE.LOW_YIELD_ITEM.DOG;
+        } else if (randomVal > 299 && randomVal <= 449) {
+            result = BET_ITEM_TYPE.LOW_YIELD_ITEM.ELEPHANT;
+        } else if (randomVal > 449 && randomVal <= 599) {
+            result = BET_ITEM_TYPE.LOW_YIELD_ITEM.ORCA;
+        } else if (randomVal > 599 && randomVal <= 749) {
+            result  = BET_ITEM_TYPE.HIGH_YIELD_ITEM.FOX;
+        } else if (randomVal > 749 && randomVal <= 899) {
+            result = BET_ITEM_TYPE.HIGH_YIELD_ITEM.SNAKE;
+        } else if (randomVal > 899 && randomVal <= 970) {
+            result = BET_ITEM_TYPE.HIGH_YIELD_ITEM.JAGUAR;
+        } else {
+            result = BET_ITEM_TYPE.HIGH_YIELD_ITEM.LION;
+        }
+
+        return result;
+    },
+    getResultTypeTwo() {
+        // high yield lower bandwith have almost as many as low yield
+        let randomVal = getRandomInt(gameSetting.MAX_RANDOMIZED);
+        let result = '';
+
+        if (randomVal <= 50) {
+            result = BET_ITEM_TYPE.LOW_YIELD_ITEM.BULL;
+        } else if (randomVal > 50 && randomVal <= 100) {
+            result = BET_ITEM_TYPE.LOW_YIELD_ITEM.DOG;
+        } else if (randomVal > 100 && randomVal <= 150) {
+            result = BET_ITEM_TYPE.LOW_YIELD_ITEM.ELEPHANT;
+        } else if (randomVal > 150 && randomVal <= 200) {
+            result = BET_ITEM_TYPE.LOW_YIELD_ITEM.ORCA;
+        } else if (randomVal > 200 && randomVal <= 400) {
+            result  = BET_ITEM_TYPE.HIGH_YIELD_ITEM.FOX;
+        } else if (randomVal > 400 && randomVal <= 600) {
+            result = BET_ITEM_TYPE.HIGH_YIELD_ITEM.SNAKE;
+        } else if (randomVal > 600 && randomVal <= 800) {
+            result = BET_ITEM_TYPE.HIGH_YIELD_ITEM.JAGUAR;
+        } else {
+            result = BET_ITEM_TYPE.HIGH_YIELD_ITEM.LION;
+        }
+
+        return result;
+    },
+    getNextDrawTime() {
+        return new Date().getTime() + this.drawTimer;
+    },
+    pickResultType: function() {
+        let random = getRandomInt(gameSetting.MAX_RANDOMIZED);
+        if (random <= 400) {
+            this.getResultType = 0;
+        } else if (random > 400 && random <= 930) {
+            this.getResultType = 1;
+        } else {
+            this.getResultType = 2;
+        }
+    },
+    pickItemType : function() {
+        let result = '';
+
+        if (this.roundCount > this.roundCap) {
+
+            // if rouncCount over rouncCap switch resultType
+            this.pickResultType();
+
+            if (this.getResultType === 0 || this.getResultType === 1) {
+                let roundCap = getRandomInt(20) + 10; 
+                this.roundCap = roundCap;
+                // resset roundCount
+                this.roundCount  = 0;
+            } else {
+                let roundCap = getRandomInt(5) + 2;
+                this.roundCap = roundCap;
+                this.roundCount = 0;
+            }
+        }
+       
+        // get result base on resultType
+        if (this.getResultType === 0) {
+            result = this.getResultTypeZero();
+        } else if (this.getResultType === 1) {
+            result = this.getResultTypeOne();
+        } else {
+            result = this.getResultTypeTwo();
+        }
+
+        // increment roundCount
+        this.roundCount = this.roundCount + 1;
+
+        
         return result;
     },
     draw: function() {
